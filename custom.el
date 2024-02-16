00688d5daa2f9d283792d7ec214f10bd7e44fb1d
William Golden <wtgolden5@gmail.com>
2024-02-16 16:07:31 -0500

    update with latest

---
+++ custom.el
@@ -0,0 +1,33 @@
+
+
+;;; custom.el -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ron-mode))
+ '(warning-suppress-log-types '((lsp-mode) (defvaralias)))
+ '(warning-suppress-types '((lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ );;; custom.el -*- lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ron-mode))
+ '(warning-suppress-log-types '((lsp-mode) (defvaralias)))
+ '(warning-suppress-types '((lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
